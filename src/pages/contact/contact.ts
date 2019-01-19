85f18ac81bc172a96bca4a83014cb5ba5ede4284
Mario Martin <mariomarsan7@gmail.com>
2019-01-19 14:02:57 +0100

    Init ionic app

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  users;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+    this.http.get('https://randomuser.me/api?results=10').subscribe((users) => {
+      console.log(users);
+      this.users = users['results'];
+    });
   }
 
 }
